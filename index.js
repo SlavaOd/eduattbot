d0d863311fef69d4b5025243100d3e17eb3fb17a
SlavaOd <sbodryan1992@gmail.com>
2020-10-05 15:00:32 +0300

    edduattbot_v1.2

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-  const TelegramBot = require('node-telegram-bot-api');
-      request = require('request');
-      fs = require('fs');
-      token = '1199928100:AAH8o35xpYvh1yPcOLiVEv0XHRBgD_7vLA4';
-      bot = new TelegramBot(token, {polling: true});
+const TelegramBot = require('node-telegram-bot-api');
+    request = require('request');
+    fs = require('fs');
+    token = '1199928100:AAH8o35xpYvh1yPcOLiVEv0XHRBgD_7vLA4';
+    bot = new TelegramBot(token, {polling: true});
 
 
 
@@ -11,7 +11,7 @@ bot.on('message', msg => {
   var txt = msg.text;
 
 
-  bot.sendMessage(chatId, 'Выполняю...', {
+  bot.sendMessage(chatId, {
     reply_markup: {
       keyboard: [
         ['Курс №1'],
@@ -20,58 +20,56 @@ bot.on('message', msg => {
       ]
     }
   })
+  switch(txt) {
+    case 'Курс №1':
+      bot.sendMessage(chatId, 'Вы выбрали Курс №1', {
+        reply_markup: {
+          keyboard: [
+            ['Лекция №1'],
+            ['Лекция №2'],
+            ['Лекция №3'],
+            ['Вернуться']
+          ]
+        }
+      });
+      break;
+    case 'Курс №2':
+      bot.sendMessage(chatId, 'Вы выбрали Курс №2', {
+        reply_markup: {
+          keyboard: [
+            ['Лекция №1'],
+            ['Лекция №2'],
+            ['Лекция №3'],
+            ['Вернуться']
+          ]
+        }
+      });
+      break;
+    case 'Курс №3':
+      bot.sendMessage(chatId, 'Вы выбрали Курс №3', {
+        reply_markup: {
+          keyboard: [
+            ['Лекция №1'],
+            ['Лекция №2'],
+            ['Лекция №3'],
+            ['Вернуться']
+          ]
+        }
+      });
+      break;
 
-
-    switch(txt) {
-      case 'Курс №1':
-        bot.sendMessage(chatId, 'Вы выбрали Курс №1', {
-          reply_markup: {
-            keyboard: [
-              ['Лекция №1'],
-              ['Лекция №2'],
-              ['Лекция №3'],
-              ['Вернуться']
-            ]
-          }
-        });
-        break;
-      case 'Курс №2':
-        bot.sendMessage(chatId, 'Вы выбрали Курс №2', {
-          reply_markup: {
-            keyboard: [
-              ['Лекция №1'],
-              ['Лекция №2'],
-              ['Лекция №3'],
-              ['Вернуться']
-            ]
-          }
-        });
-        break;
-      case 'Курс №3':
-        bot.sendMessage(chatId, 'Вы выбрали Курс №3', {
-          reply_markup: {
-            keyboard: [
-              ['Лекция №1'],
-              ['Лекция №2'],
-              ['Лекция №3'],
-              ['Вернуться']
-            ]
-          }
-        });
-        break;
-
-      case 'Вернуться':
-        bot.sendMessage(chatId, 'ok', {
-          reply_markup: {
-            keyboard: [
-              ['Курс №1'],
-              ['Курс №2'],
-              ['Курс №3']
-            ]
-          }
-        })
-    }    
-}  )
+    case 'Вернуться':
+      bot.sendMessage(chatId, 'ok', {
+        reply_markup: {
+          keyboard: [
+            ['Курс №1'],
+            ['Курс №2'],
+            ['Курс №3']
+          ]
+        }
+      })
+  }    
+})
 
 
 
